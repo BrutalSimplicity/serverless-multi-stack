230317ee8d38b81ec6f15f2102967bcf0259bcfe
Kory Taborn <kory.taborn@gmail.com>
2020-02-23 18:58:11 -0600

    add unit/integration tests

--- tests/serverless-multi-stack-plugin.tests.ts
+++ tests/serverless-multi-stack-plugin.tests.ts
@@ -1,11 +1,62 @@
-import { expect } from 'chai';
-import { toConfig } from '../src/models';
 import { AssertionError } from 'assert';
-import Serverless from 'serverless';
-import PluginManager from 'serverless/classes/PluginManager';
+import { expect } from 'chai';
 import fs from 'fs';
-import _ from 'lodash';
 import yaml from 'js-yaml';
+import _ from 'lodash';
+import '../src/lodash-async';
+import Serverless from 'serverless';
+import sinon from 'sinon';
+import { MultiStackConfig, toConfig } from '../src/models';
+import MultiStackPlugin from '../src/serverless-multi-stack';
+
+const multiStack = {
+  service: 'multi-stack',
+  provider: {
+    name: 'aws'
+  },
+  custom: {
+    'multi-stack': {
+      stacks: {
+        'serverless.1.yml': {
+          setting1: true,
+          setting2: false,
+          beforeRemove: {
+            shell: 'echo test'
+          }
+        },
+        'serverless.2.yml': {},
+        'serverless.3.yml': {
+          afterDeploy: {
+            handler: 'afterDeploy.handler'
+          }
+        }
+      },
+      regions: {
+        'us-east-1': {}
+      }
+    } as MultiStackConfig
+  }
+};
+const stacks = {
+  'serverless.1.yml': {
+    service: 'service-1',
+    provider: {
+      name: 'aws'
+    }
+  },
+  'serverless.2.yml': {
+    service: 'service-2',
+    provider: {
+      name: 'aws'
+    }
+  },
+  'serverless.3.yml': {
+    service: 'service-3',
+    provider: {
+      name: 'aws'
+    }
+  }
+};
 
 describe('MultiStackPlugin', function () {
   describe('should throw when config is invalid', function () {
@@ -54,54 +105,44 @@ describe('MultiStackPlugin', function () {
     })
   })
 
-  describe('should return valid config', function () {
-    const multiStack = {
-      service: 'multi-stack',
-      provider: {
-        name: 'aws'
-      },
-      custom: {
-        'multi-stack': {
-          'stacks': {
-            'serverless.1.yml': {
-              setting1: 'true',
-              setting2: 'false',
-              afterDestroy: {
-                shell: 'echo destroyed'
-              }
-            },
-            'serverless.2.yml': null as any,
-            'serverless.3.yml': {
-              afterDeploy: {
-                handler: 'afterDesploy.handler'
-              }
-            }
-          },
-          'regions': {
-            'us-east-1': null as any,
-          }
-        }
-      }
-    };
-    const stacks = {
-      'serverless.1.yml': {
-        service: 'service-1'
-      },
-      'serverless.2.yml': {
-        service: 'service-1'
-      },
-      'serverless.3.yml': {
-        service: 'service-1'
-      }
-    };
+  describe('lodash-async', function() {
+    it('should return result from async flow', async function() {
+      type TestType = { a: string, x: number, y: number } | {};
 
-    const config = 'serverless.yml';
+      const async1 = _.curry((state: TestType): Promise<TestType> => {
+        return Promise.resolve({ ...state, a: 'aa' });
+      })
+      const async2 = _.curry((state: TestType): Promise<TestType> => {
+        return Promise.resolve(state);
+      })
+      const async3 = _.curry((state: any): Promise<TestType> => {
+        _(state).forOwn((v, k) => {
+          if (_.isNumber(v)) {
+            state[k] = v * 2;
+          }
+        })
+        return Promise.resolve(state);
+      })
+
+      const actual = await _.flowAsync(
+        async1,
+        async2,
+        async3
+      )({ a: 'a', x: 1, y: 2 });
+
+      const expected = { a: 'aa', x: 2, y: 4 };
+
+      expect(actual).to.deep.equal(expected);
+    })
+  })
+
+  describe('validate serverless plugin pipeline', function () {
     const servicePath = process.cwd();
-    const serverless = new Serverless({ servicePath });
+    const config = 'serverless.yml';
     const options = { stage: 'dev', region: 'us-east-1', config };
+    const serverless = new Serverless({ servicePath });
 
     before('create serverless stacks', async function () {
-
       _(stacks).forOwn((stack, location) => {
         fs.writeFileSync(location, yaml.safeDump(stack), { flag: 'w+' });
       });
@@ -109,17 +150,53 @@ describe('MultiStackPlugin', function () {
       fs.writeFileSync('serverless.yml', yaml.safeDump(multiStack), { flag: 'w+' });
 
       serverless.processedInput = {
-        commands: [],
+        commands: {},
         options
       };
       await serverless.service.load(options);
     })
 
-    it('should return valid stack and region references', function () {
-      expect(serverless.service.custom?.['multi-stack']).to.not.be.null;
-      const config = toConfig(serverless.service.custom['multi-stack']);
+    it('should throw when region stack does not exist', function() {
+      const stacks = _(multiStack).cloneDeep();
+      stacks.custom["multi-stack"].regions['us-east-1']['serverless.4.yml'] = {};
 
-      console.log(config);
+      expect(() => toConfig(stacks.custom['multi-stack'])).to.throw(AssertionError);
+    })
+
+    it('should map valid MultiStackConfig', function () {
+      expect(serverless.service.custom?.['multi-stack']).to.not.be.null;
+
+      const config = toConfig(serverless.service.custom['multi-stack']);
+      expect(config).to.deep.equal(multiStack.custom['multi-stack']);
+    })
+
+    it('should have valid plugin configuration', function() {
+      const plugin = new MultiStackPlugin(serverless, options);
+      const hooks = plugin.hooks;
+
+      expect(hooks['after:deploy:deploy'].name).to.include(plugin.deployStacks.name);
+      expect(hooks['before:remove:remove'].name).to.include(plugin.removeStacks.name);
+    })
+
+    it('should call deploy for each stack', async function() {
+      const plugin = new MultiStackPlugin(serverless, options);
+
+      const stacks = [] as Serverless[];
+      serverless.pluginManager.spawn = (__) => { stacks.push(_(serverless).cloneDeep()); return Promise.resolve() };
+      serverless.cli = {
+        log: (_) => {},
+        setLoadedCommands: (_) => {},
+        setLoadedPlugins: (_) => {}
+      };
+
+      await plugin.deployStacks();
+
+      expect(stacks).length(3);
+      _(stacks)
+      .zip(['service-1', 'service-2', 'service-3'])
+      .forEach(pair => {
+        expect(pair[0].service.getServiceName()).to.equal(pair[1]);
+      })
     })
 
     after('destroy serverless stacks', function () {
